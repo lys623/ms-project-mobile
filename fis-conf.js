7a0a9834c3212d43d06732206e17803a9f75c477
lys623 <lys623@qq.com>
2015-10-30 19:07:46 +0800

    修改配置文件

---
+++ fis-conf.js
@@ -0,0 +1,43 @@
+fis.hook('relative');
+fis.match('**', {
+    relative: true
+});
+
+
+fis.match('::packager', {
+    postpackager: fis.plugin('loader', {
+        allInOne:{
+            css: 'css/index.css',
+            js: 'js/index.js',
+        }
+    })
+})
+
+fis.match('*.html', {
+    postprocessor:fis.plugin('format')
+})
+
+// 启用 fis-spriter-csssprites 插件
+fis.match('::package', {
+    spriter: fis.plugin('csssprites')
+})
+fis.match('**/(*.png)', {
+    release:'css/i/$1'
+})
+
+fis.match('**/(*.jpg)', {
+    release:'css/i/$1'
+})
+// 对 CSS 进行图片合并
+fis.match('*.{css,less}', {
+    // 给匹配到的文件分配属性 `useSprite`
+    useSprite: true,
+    parser: fis.plugin('cssnext')
+});
+fis.match('*.less', {
+    rExt: '.css',
+    parser: fis.plugin('less-2.x', {
+    })
+}).match('*.png', {
+    optimizer: fis.plugin('png-compressor')
+})
